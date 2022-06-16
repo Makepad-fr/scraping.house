a3bbfaaea8ff741edfc6d148c2314e47552f9681
kaanyagci <kaan.yagci@clique.paris>
2022-06-17 00:29:44 +0200

    fix(config): base module out folder removed

--- modules/base/out/commons.d.ts
+++
@@ -1,3 +0,0 @@
-import AbstractModule, { ModuleOptions, ModuleInitializationOptions } from './abstract-module';
-export { AbstractModule, ModuleOptions, ModuleInitializationOptions };
-//# sourceMappingURL=commons.d.ts.map
--- modules/base/out/commons.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AbstractModule = void 0;
-const abstract_module_1 = __importDefault(require("./abstract-module"));
-exports.AbstractModule = abstract_module_1.default;
-//# sourceMappingURL=commons.js.map
