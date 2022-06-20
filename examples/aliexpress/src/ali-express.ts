dc457fd3bda68ae82fa4a45de86eefc2d82710ca
kaanyagci <kaan.yagci@clique.paris>
2022-06-20 02:34:44 +0200

    feat(example): AliExpress example updated
    Getting category list and sub categories added

--- examples/aliexpress/src/ali-express.ts
+++ examples/aliexpress/src/ali-express.ts
@@ -1,1 +1,22 @@
-console.log('Hello world');
+import AliExpress from '@scraping.house/aliexpress';
+
+async function main() {
+  const aliExpress = await AliExpress.init({
+    browser: 'firefox',
+    launchOptions: {
+      headless: false
+    },
+    contextOptions: undefined,
+    authenticatedContextPath: './aliexpress.json'
+  });
+  await aliExpress.handleCookies('accept');
+  const aliExpressCategoryList = await aliExpress.categoryList();
+  await aliExpressCategoryList.getAll();
+  // TODO: Get the list of all categories
+
+  // TODO: Accept/refuse notifications
+  // TODO: Save browser path
+  // TODO: Get list of all categories
+}
+
+main().then();
